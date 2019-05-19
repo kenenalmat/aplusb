e9bf4ddf006c8d28ccd80a202a0fc1b72c22a745
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-05-20 01:23:17 +0600

    return jsonify

--- src/assignment_1/task_1/app.py
+++ src/assignment_1/task_1/app.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from flask import request
+from flask import request, Response, jsonify
 
 app = Flask(__name__)
 
-@app.route('/task_1')
-def server(methods=["POST"]):
+@app.route('/', methods=["POST"])
+def server():
     # might be request.args.get('a')
     a = int(request.form['a'])
     b = int(request.form['b'])
     # a = int(request.args.get('a'))
     # b = int(request.args.get('b'))
-    return str(solve(a, b))
+    ans = {'result': solve(a, b)}
+    print ("GOT THE RESULT: {}".format(ans['result']))
+    return jsonify(ans)
+
 
 def solve(a, b):
     return a + b
